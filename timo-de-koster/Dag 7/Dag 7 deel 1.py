bfcdeeec31aa066b7eba45d122ed667624e72025
Timo de Koster <t.dekoster@vcareconnect.nl>
2025-12-07 20:02:23 +0100

    Dag 7 deel 1

---
+++ timo-de-koster/Dag 7/Dag 7 deel 1.py
@@ -0,0 +1,33 @@
+beamCoords = []
+splits = 0
+
+with open('input.txt', 'r') as file:
+    
+    first = file.readline()
+    beamCoords.append(first.find('S'))
+
+    for line in file:
+        newCoords = set()
+        removedCoords = []
+
+        for coord in beamCoords:
+            if(line[coord] == '^'):
+                removedCoords.append(coord)
+                left = coord - 1
+                right = coord + 1
+
+                beamSplit = False
+
+                if not beamCoords.__contains__(left):
+                    newCoords.add(left)
+                if not beamCoords.__contains__(right):
+                    newCoords.add(right)
+                
+                splits += 1
+        
+        for coord in removedCoords:
+              beamCoords.remove(coord)
+        for coord in newCoords:
+             beamCoords.append(coord)
+                    
+print(f'Total splits: {splits}')
